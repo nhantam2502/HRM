d3670ec260ca3deabc74d6dd985b17fb85b7b797
DatPham1103 <phamtdat1103@gmail.com>
2024-01-10 20:48:15 +0700

    update table

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,14 +1,34 @@
-import { Button, Input, Layout, Popover, Select, Table, Tooltip, Typography } from "antd";
+import {
+  Button,
+  Input,
+  Layout,
+  Popover,
+  Select,
+  Table,
+  Tooltip,
+  Typography,
+} from "antd";
 import PropTypes from "prop-types";
-import setImage from "../assets/logo-color.png"
+import setImage from "../assets/logo-color.png";
 const { Header, Sider } = Layout;
 import { Content } from "antd/es/layout/layout";
 import SidebarMenu from "./SidebarMenu/SidebarMenu";
 import HeaderMenu from "./Header/Header";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { AlignCenterOutlined, ClockCircleOutlined, DeleteOutlined, DownloadOutlined, EditOutlined, MenuFoldOutlined, MenuUnfoldOutlined, PlusOutlined, SearchOutlined, TeamOutlined } from "@ant-design/icons";
-import "../styles/Layout.css"
+import {
+  AlignCenterOutlined,
+  ClockCircleOutlined,
+  DeleteOutlined,
+  DownloadOutlined,
+  EditOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PlusOutlined,
+  SearchOutlined,
+  TeamOutlined,
+} from "@ant-design/icons";
+import "../styles/Layout.css";
 
 const { Title } = Typography;
 
@@ -25,13 +45,19 @@ function getItem(label, key, icon, children, type) {
 const items_for_test = [
   getItem("Thông tin nhân sự", "sub1", <TeamOutlined />, [
     getItem("Hợp đồng lao động nhân viên"),
-    getItem("Quản lý quá trình", "g1", null, [
-      getItem("+ Quá trình đào tạo", "1"),
-      getItem("+ Quá trình công tác", "2"),
-      getItem("+ Quá trình hoạt động đoàn viên", "3"),
-      getItem("+ Quá trình khen thưởng - kỷ luật", "4"),
-      getItem("+ Thuế thu nhập cá nhân")
-    ], 'group'),
+    getItem(
+      "Quản lý quá trình",
+      "g1",
+      null,
+      [
+        getItem("+ Quá trình đào tạo", "1"),
+        getItem("+ Quá trình công tác", "2"),
+        getItem("+ Quá trình hoạt động đoàn viên", "3"),
+        getItem("+ Quá trình khen thưởng - kỷ luật", "4"),
+        getItem("+ Thuế thu nhập cá nhân"),
+      ],
+      "group"
+    ),
     getItem("Báo cáo thống kê hoạt động nhân sự"),
     getItem("Xuất dữ liệu"),
   ]),
@@ -43,16 +69,21 @@ const items_for_test = [
     getItem("Báo cáo quá trình làm việc"),
   ]),
 
-  getItem("Tính lương & các khoản giảm trừ theo lương", "sub3", <TeamOutlined />, [
-    getItem("Khai báo hệ số tiền lương và mức lương"),
-    getItem("Lương theo thời gian"),
-    getItem("Khai báo tăng lương"),
-    getItem("Lương công nhật"),
-    getItem("Lương sản phẩm"),
-    getItem("Khai báo công thức tính các khoản giảm trừ"),
-    getItem("Báo cáo tổng lương"),
-    getItem("Báo cáo phân bổ lương sản phẩm"),
-  ]),
+  getItem(
+    "Tính lương & các khoản giảm trừ theo lương",
+    "sub3",
+    <TeamOutlined />,
+    [
+      getItem("Khai báo hệ số tiền lương và mức lương"),
+      getItem("Lương theo thời gian"),
+      getItem("Khai báo tăng lương"),
+      getItem("Lương công nhật"),
+      getItem("Lương sản phẩm"),
+      getItem("Khai báo công thức tính các khoản giảm trừ"),
+      getItem("Báo cáo tổng lương"),
+      getItem("Báo cáo phân bổ lương sản phẩm"),
+    ]
+  ),
   //   getItem("Collaborators & Teams", "collaborators", <TeamOutlined />),
   //   getItem("My Task", "task", <SolutionOutlined />),
 ];
@@ -63,7 +94,6 @@ const DefaultLayout = ({ children }) => {
   const refreshTable = false;
   const navigate = useNavigate();
 
-
   const handleHomePage = () => {
     navigate("/homepage");
   };
@@ -72,52 +102,55 @@ const DefaultLayout = ({ children }) => {
     {
       title: "Intern ID",
       dataIndex: "internId",
-
+      width:120,
+      fixed:'left'
     },
     {
       title: "Date submitted",
       dataIndex: "dateSubmitted",
-
+      width:120,
+      fixed:'left'
     },
     {
       title: "Full name",
       dataIndex: "fullName",
+      width:150,
 
     },
     {
       title: "DoB",
       dataIndex: "dob",
-
+      width:120,
     },
     {
       title: "Phone number",
       dataIndex: "phoneNumber",
-
+      width:120,
     },
     {
       title: "Position",
       dataIndex: "position",
-
+      width:200,
     },
     {
       title: "School",
       dataIndex: "school",
-
+      width:100,
     },
     {
       title: "Address",
       dataIndex: "address",
-
+      width:200,
     },
     {
       title: "Email",
       dataIndex: "email",
-
+      width:200,
     },
     {
       title: "CV",
       dataIndex: "cv",
-
+      width:80,
       render: (text, record) => (
         <a href={text} target="_blank">
           View
@@ -127,7 +160,7 @@ const DefaultLayout = ({ children }) => {
     {
       title: "Comment",
       dataIndex: "comment",
-
+      width:100,
       render: (text, record) => (
         <Popover
           content={<span style={{ whiteSpace: "pre-wrap" }}>{text}</span>}
@@ -141,7 +174,7 @@ const DefaultLayout = ({ children }) => {
     {
       title: "Status",
       dataIndex: "status",
-
+      width:140,
       render: (text, record) => (
         <Select defaultValue="Pending">
           <Select.Option value="accept" style={{ color: "green" }}>
@@ -159,11 +192,12 @@ const DefaultLayout = ({ children }) => {
     {
       title: "Buttons",
       dataIndex: "buttons",
-
+      fixed:'right',
+      width:240,
       render: (text, record) => (
         <div>
-          <Button style={{width:'100px', marginRight:'5px'}}>View</Button>
-          <Button style={{width:'100px'}}>Comment</Button>
+          <Button style={{ width: "100px", marginRight: "5px" }}>View</Button>
+          <Button style={{ width: "100px" }}>Comment</Button>
         </div>
       ),
     },
@@ -243,7 +277,7 @@ const DefaultLayout = ({ children }) => {
 
   const buttonStyle = {
     margin: "0 10px",
-    width:'120px'
+    width: "120px",
   };
   const searchButton = {
     flexBasis: "33%",
@@ -336,7 +370,6 @@ const DefaultLayout = ({ children }) => {
                         height: 64,
                       }}
                     />
-
                   </div>
                   <HeaderMenu></HeaderMenu>
                 </div>
@@ -354,183 +387,186 @@ const DefaultLayout = ({ children }) => {
               </Content> */}
 
               <Content style={{ margin: "5px 16px" }}>
-            <div
-              className="topContentContainer"
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                padding: 20,
-                minHeight: 30,
-              }}
-            >
-              <div>
-              <Title level={2} style={{margin:'0'}}>Approve CV</Title>
-              </div>
-            </div>
-          </Content>
-
-          <Content style={{ margin: "10px 16px" }}>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-                padding: 24,
-                minHeight: 100,
-                borderRadius: 30,
-              }}
-            >
-              <div
-                style={{ display: "flex", alignItems: "center"}}
-              >
-                <Title level={3} style={{color:'grey',margin:'0'}}>Search for information</Title>
-              </div>
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <Button
-                  size="large"
-                  className="scheduleBtn"
-                  type="primary"
-                  icon={<ClockCircleOutlined />}
-                  style={buttonStyle}
-                >
-                  Schedule
-                </Button>
-                <Button
-                  size="large"
-                  className="exportBtn"
-                  type="primary"
-                  icon={<DownloadOutlined />}
-                  style={buttonStyle}
-                >
-                  Export
-                </Button>
-                <Button
-                  size="large"
-                  className="editBtn"
-                  type="primary"
-                  icon={<EditOutlined />}
-                  style={buttonStyle}
-                >
-                  Edit
-                </Button>
-                <Button
-                  size="large"
-                  className="deleteBtn"
-                  type="primary"
-                  icon={<DeleteOutlined />}
-                  style={buttonStyle}
-                >
-                  Delete
-                </Button>
-                <Button
-                  size="large"
-                  className="addBtn"
-                  type="primary"
-                  icon={<PlusOutlined />}
-                  style={buttonStyle}
-                >
-                  Add
-                </Button>
-              </div>
-            </div>
-          </Content>
-          
-          <Content style={{ margin: "0 16px" }}>
-            <div
-              style={{
-                padding: 24,
-                minHeight: 360,
-                borderRadius: 30,
-              }}
-            >
-              <div className="searchContainer" style={{ display: "flex" }}>
                 <div
-                  className="searchBar"
+                  className="topContentContainer"
                   style={{
                     display: "flex",
-                    flexWrap: "wrap",
                     justifyContent: "space-between",
+                    padding: 20,
+                    minHeight: 30,
                   }}
                 >
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
-                  <Input
-                    size="small"
-                    placeholder="Basic usage"
-                    style={searchButton}
-                  />
+                  <div>
+                    <Title level={2} style={{ margin: "0" }}>
+                      Approve CV
+                    </Title>
+                  </div>
                 </div>
+              </Content>
+
+              <Content style={{ margin: "10px 16px" }}>
                 <div
-                  className="searchButton"
-                  style={{ marginLeft: "10px", display: "grid" }}
-                >
-                  <Button icon={<AlignCenterOutlined />}>Filter</Button>
-                  <Button type="primary" icon={<SearchOutlined />}>
-                    Search
-                  </Button>
-                </div>
-              </div>
-
-              <div className="tableContainer" style={{ marginTop: "20px" }}>
-                <Table
-                  className="tableLayout"
-                  tableLayout="auto"
-                  rowSelection={{
-                    type: "checkbox",
-                    ...rowSelection,
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    padding: 24,
+                    minHeight: 100,
+                    borderRadius: 30,
                   }}
-                  columns={columns}
-                  dataSource={data}
-                />
-              </div>
-            </div>
-          </Content>
+                >
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <Title level={3} style={{ color: "grey", margin: "0" }}>
+                      Search for information
+                    </Title>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <Button
+                      size="large"
+                      className="scheduleBtn"
+                      type="primary"
+                      icon={<ClockCircleOutlined />}
+                      style={buttonStyle}
+                    >
+                      Schedule
+                    </Button>
+                    <Button
+                      size="large"
+                      className="exportBtn"
+                      type="primary"
+                      icon={<DownloadOutlined />}
+                      style={buttonStyle}
+                    >
+                      Export
+                    </Button>
+                    <Button
+                      size="large"
+                      className="editBtn"
+                      type="primary"
+                      icon={<EditOutlined />}
+                      style={buttonStyle}
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      size="large"
+                      className="deleteBtn"
+                      type="primary"
+                      icon={<DeleteOutlined />}
+                      style={buttonStyle}
+                    >
+                      Delete
+                    </Button>
+                    <Button
+                      size="large"
+                      className="addBtn"
+                      type="primary"
+                      icon={<PlusOutlined />}
+                      style={buttonStyle}
+                    >
+                      Add
+                    </Button>
+                  </div>
+                </div>
+              </Content>
 
+              <Content style={{ margin: "0 16px" }}>
+                <div
+                  style={{
+                    padding: 24,
+                    minHeight: 360,
+                    borderRadius: 30,
+                  }}
+                >
+                  <div className="searchContainer" style={{ display: "flex" }}>
+                    <div
+                      className="searchBar"
+                      style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                      <Input
+                        size="small"
+                        placeholder="Basic usage"
+                        style={searchButton}
+                      />
+                    </div>
+                    <div
+                      className="searchButton"
+                      style={{ marginLeft: "10px", display: "grid" }}
+                    >
+                      <Button icon={<AlignCenterOutlined />}>Filter</Button>
+                      <Button type="primary" icon={<SearchOutlined />}>
+                        Search
+                      </Button>
+                    </div>
+                  </div>
+
+                  <div className="tableContainer" style={{ marginTop: "20px" }}>
+                    <Table
+                      className="tableLayout"
+                      scroll={{
+                        x: 2000,
+                        y:100
+                      }}
+                      rowSelection={{
+                        type: "checkbox",
+                        ...rowSelection,
+                      }}
+                      columns={columns}
+                      dataSource={data}
+                    />
+                  </div>
+                </div>
+              </Content>
             </Layout>
-
           </Layout>
         </>
       )}
